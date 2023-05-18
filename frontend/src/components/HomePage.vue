76c8a4b8c5ec80cffdcc3903582dae512d3dac87
nicoloeffe <nfiordaligi@gmail.com>
2023-05-18 13:50:47 +0200

    frontend structure with vue+tailwind and homepage implementation

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,20 @@
+
+<template>
+<section class="relative">
+  <img src="../images/sfondo.jpeg" alt="Background Image" class="w-full h-full object-cover">
+  <div class="absolute inset-0 flex justify-center items-center">
+    <div class="flex flex-col justify-center items-center">
+      <button class="bg-black text-white rounded my-4 py-2 px-4 w-full">Prenota un campo!</button>
+      <button class="bg-green-500 rounded text-black my-4 py-2 px-4 w-full">Login</button>
+      <button class="bg-green-500 rounded text-black my-4 py-2 px-4 w-full">Registrati</button>
+    </div>
+  </div>
+</section>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'HomePage'
+})
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    <LogoBar/>
+    <StartPage/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LogoBar from '@/components/LogoBar.vue'
+import StartPage from '@/components/HomePage.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    LogoBar, 
+    StartPage
+  }
+}
+</script>
