d69aa373edc353591e77392c1e287f34bdb5d166
Milena Sonda <milena.sonda@studenti.unitn.it>
2023-06-10 15:25:38 +0200

    fixes permessi

--- backend/server.js
+++ backend/server.js
@@ -3,52 +3,38 @@ const cors = require('cors');
 const config = require('./config');
 
 // connect to DB 
-const db= require('./models');
+const db = require('./models');
 
 // import all ROUTES
-const authRoute = require ('./routes/autenticazione');
+const authRoute = require('./routes/autenticazione');
 const prenotazioniRouter = require('./routes/prenotazioni');
 const campiRouter = require('./routes/campi');
 
-const app=express();
+const app = express();
 
-var corsOptions={
-    origin:"http://localhost:8081"
-}
+var corsOptions = {
+  origin: config.FRONT_PORT
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Use Controllers
 app.use("/auth", authRoute);
 app.use("/campi", campiRouter)
 app.use("/prenotazioni", prenotazioniRouter);
 
+// Handle non-existing routes
+app.use('/*', (req, res) => {
+  res.status(404).json({ success: false, message: "Route inesistente" });
+});
 
-// Gestisci le pagine che non esistono
-app.use((req, res) => {
-    res.status(404).json({ success: false, message: "Route inesistente" });
-  });
-
-const connectDB = require('./connectDB')
-
-connectDB()
-
-// controllare questo URI, nel senso che mondodb docs dice di fare un URI cosi
-// mongodb://username:password@127.0.0.1:27017/<nome_db>?options
-// db.mongoose.connect(process.env.MONGODB_CONNECT_URI,{
-//     useUnifiedTopology: true
-// }).then(()=>{
-//     console.log("Successfully connected to mongoDb")
-// }).catch(err=>{
-//     console.log(err);
-//     process.exit();
-// })
+const connectDB = require('./connectDB');
+connectDB();
 
 const PORT = config.PORT;
-const HOST = config.HOST;
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
