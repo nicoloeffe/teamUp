38fc79435e50ad88e53153ddce44e524fee4d845
giuliomuraro <giuliomuraro97@gmail.com>
2023-06-11 20:43:12 +0200

    Fix test

--- backend/test/prenotazione.test.js
+++ backend/test/prenotazione.test.js
@@ -46,4 +46,30 @@ describe('Test prenotazioni', () => {
     expect(res.status).toBe(404);
     expect(res.body).toEqual({ success: false, message: "Impossibile creare la prenotazione: Utente non trovato" });
   });
+
+
+  test('POST prenotazione ok', async () => {
+    const res = await request(app).post('/prenotazioni').set('Content-Type', 'application/json').send({
+      nome: mockData.state.campo.nome,
+      data: mockData.state.prenotazione.data,
+      orario: mockData.state.prenotazione.orario,
+      utente: mockData.state.users
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('POST orari prenotazioni utente ok', async () => {
+    const res = await request(app).post('/prenotazioni/getprenotazioni').set('Content-Type', 'application/json').send({
+      utente: mockData.state.users
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('POST orari prenotazioni utente fasullo', async () => {
+    const res = await request(app).post('/prenotazioni/getprenotazioni').set('Content-Type', 'application/json').send({
+      utente: {email: "email fasulla"}
+    });
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({ success: false, message: "Impossibile trovare l'utente" });
+  });
 });
